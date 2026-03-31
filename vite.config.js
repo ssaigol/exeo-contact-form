b7d00dc5e7bcc18087275b0ccfea7a7fcb694936
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2026-03-31 13:30:43 -0400

    Fix add memoization to country list dropdown, add sentry sourcemapping

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 export default defineConfig({
+  build: { sourcemap: "hidden" },
   base: "./",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sentryVitePlugin({
+      org: "aldisa-global-inc",
+      project: "exeo-apps-portal-frontend-react",
+      sourcemaps: {
+        filesToDeleteAfterUpload: ["./dist/**/*.map"],
+      },
+    }),
+  ],
 });
